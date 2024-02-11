a11b7dfa817c5e605c76c20329be91a37c3f22df
mahemedaziz <azizberrais@outlook.com>
2024-02-11 14:25:03 +0100

    Liking

--- Web Fundamentals/week_2/Day_1/Core/Liking Likes/script.js
+++ Web Fundamentals/week_2/Day_1/Core/Liking Likes/script.js
@@ -1,1 +1,30 @@
 console.log("Hello");
+
+console.log("Hello");
+
+var number1 = 1;
+var countElement1 = document.querySelector("#number1");
+
+function add1(){
+  number1++;
+  countElement1.innerText = number1 + " Like(s)";
+  console.log(number1);
+}
+
+var number2 = 1;
+var countElement2 = document.querySelector("#number2");
+
+function add2(){
+  number2++;
+  countElement2.innerText = number2 + " Like(s)";
+  console.log(number2);
+}
+
+var number3 = 1;
+var countElement3 = document.querySelector("#number3");
+
+function add3(){
+  number3++;
+  countElement3.innerText = number3 + " Like(s)";
+  console.log(number3);
+}
