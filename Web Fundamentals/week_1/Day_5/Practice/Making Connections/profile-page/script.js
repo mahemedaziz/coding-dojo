6e8665d4040c69be048b759a03a02346bc5352c5
mahemedaziz <azizberrais@outlook.com>
2024-02-11 15:29:49 +0100

    MAKINGconnection

--- Web Fundamentals/week_1/Day_5/Practice/Making Connections/profile-page/script.js
+++ Web Fundamentals/week_1/Day_5/Practice/Making Connections/profile-page/script.js
@@ -1,10 +1,12 @@
 console.log("page loaded...");
-console.log("page loaded...");
 
 var requestSpan = document.querySelector("#requests");
 var connectionSpan = document.querySelector("#connections");
 var username = document.querySelector("#username");
 
+function edit() {
+    username.innerText = "Aziza";
+}
 function accept(id) {
     var element = document.querySelector(id);
     element.remove();
@@ -16,8 +18,4 @@ function ignore(id) {
     var element = document.querySelector(id);
     element.remove();
     requestSpan.innerText--;
-}
-
-function edit() {
-    username.innerText = "Marisa G";
 }
