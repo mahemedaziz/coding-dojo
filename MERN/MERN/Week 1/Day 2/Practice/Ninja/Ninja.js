7e5c64f6f565062ab38e7946228165d756d17ef2
mahemedaziz <azizberrais@outlook.com>
2024-04-02 14:02:51 +0100

    MERN

---
+++ MERN/MERN/Week 1/Day 2/Practice/Ninja/Ninja.js
@@ -0,0 +1,25 @@
+class Ninja{
+    constructor(name , health , speed , strength ){
+        this.name = name;
+        this.health = 90 ;
+        this.speed = 3 ;
+        this.strength = 3
+    }
+    sayName(){
+        console.log(`the name of the Ninja ist ${ this.name}.`);
+    }
+
+    showStats(){
+        console.log(`Name: ${this.name}, Strength: ${this.strength}, Speed: ${this.speed}, Health: ${this.health}`)
+    }
+
+    drinkSake(){
+        this.health += 10
+        console.log (`${this.name} drink a Sake  and has now ${this.health} health .`)
+    }
+}
+
+const ninja1 = new Ninja("Hyabusa");
+ninja1.sayName();
+ninja1.showStats();
+ninja1.drinkSake();
