a78997188db760b7787e9e89ec1518b04b5d2a8e
mahemedaziz <azizberrais@outlook.com>
2024-05-23 17:11:42 +0100

    javas

---
+++ JAVA/Week 1/Day 3/Core/zookeeper/Mammal.java
@@ -0,0 +1,16 @@
+package com.codingdojo.zookeeper;
+
+public class Mammal {
+	
+	protected int energy;
+	
+	public Mammal() {
+        this.energy = 100;
+	}
+	
+	public int displayEnergy() {
+        System.out.println("Energy level: " + energy);
+        return energy;
+    }
+
+}
